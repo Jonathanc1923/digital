52018dfff8407e028387e025259ca7f2696149f7
Jonathanc1923 <jonathanencina2020@gmail.com>
2023-10-30 21:34:16 -0500

    Corregir Procfile y configuraciones

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+import os
 
 app = Flask(__name__)
 app.secret_key = 'tu_clave_secreta_aqui'  # Reemplaza 'tu_clave_secreta_aqui' por una clave segura.
@@ -25,4 +26,6 @@ def inicioproyecto():
         return redirect(url_for('inicio'))
 
 if __name__ == '__main__':
-    app.run()
+    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
