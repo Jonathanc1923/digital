9451dc7e7dbea645a5357f71c69583cf2ac54593
Jonathanc1923 <jonathanencina2020@gmail.com>
2023-10-30 22:10:20 -0500

    Agregada importación de os

--- app.py
+++ app.py
@@ -4,28 +4,15 @@ import os
 app = Flask(__name__)
 app.secret_key = 'as8327e9adak3iiq9'  # Reemplaza 'tu_clave_secreta_aqui' por una clave segura.
 
-# Diccionario para almacenar usuarios y contraseñas (esto es solo para demostración)
-usuarios = {
-    'usuario1@gmail.com': 'contraseña1',
-    'usuario2@gmail.com': 'contraseña2',
-}
-
 @app.route('/')
 def inicio():
     return render_template('inicio.html')
 
 @app.route('/inicioproyecto', methods=['POST'])
 def inicioproyecto():
-    correo = request.form.get('correo')
-    contraseña = request.form.get('contraseña')
-
-    if correo in usuarios and usuarios[correo] == contraseña:
-        return render_template('inicioproyecto.html')
-    else:
-        flash('Contraseña incorrecta. Por favor, inténtalo de nuevo.', 'error')
-        return redirect(url_for('inicio'))
+    return render_template('inicioproyecto.html')
 
 if __name__ == '__main__':
-    
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
